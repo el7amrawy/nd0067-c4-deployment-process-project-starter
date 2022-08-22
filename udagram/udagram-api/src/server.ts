e9b554206cacef50e8eec4c6b76bedd41f4b7810
Aly <el7amrawy@outlook.com>
2022-08-22 19:57:52 +0200

    frontend deploy

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -1,16 +1,28 @@
 import * as dotenv from "dotenv";
-import cors from 'cors';
+import cors from "cors";
 import express from "express";
-import { sequelize } from "./sequelize";
+import { Sequelize } from "sequelize-typescript";
+
+//
+// import pg from "pg";
+//
 
 import { IndexRouter } from "./controllers/v0/index.router";
 
 import bodyParser from "body-parser";
 import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
+export const sequelize = new Sequelize({
+  username: "postgres",
+  password: "postgres",
+  database: "postgres",
+  host: "database-2.cnsch3nefuvk.us-east-1.rds.amazonaws.com",
+  port: 5432,
+  dialect: "postgres",
+});
+
 (async () => {
   dotenv.config();
-
   await sequelize.addModels(V0_FEED_MODELS);
   await sequelize.addModels(V0_USER_MODELS);
   await sequelize.sync();
@@ -24,19 +36,25 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   // app.use(cors());
   // We set the CORS origin to * so that we don't need to
-  // worry about the complexities of CORS. 
-  app.use(cors({
-    "allowedHeaders": [
-      'Origin', 'X-Requested-With',
-      'Content-Type', 'Accept',
-      'X-Access-Token', 'Authorization', 'Access-Control-Allow-Origin',
-      'Access-Control-Allow-Headers',
-      'Access-Control-Allow-Methods'
-    ],
-    "methods": 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    "preflightContinue": true,
-    "origin": '*',
-  }));
+  // worry about the complexities of CORS.
+  app.use(
+    cors({
+      allowedHeaders: [
+        "Origin",
+        "X-Requested-With",
+        "Content-Type",
+        "Accept",
+        "X-Access-Token",
+        "Authorization",
+        "Access-Control-Allow-Origin",
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Methods",
+      ],
+      methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+      preflightContinue: true,
+      origin: "*",
+    })
+  );
 
   app.use("/api/v0/", IndexRouter);
 
